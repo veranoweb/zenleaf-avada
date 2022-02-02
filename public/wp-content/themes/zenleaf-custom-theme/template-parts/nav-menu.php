886b76305817192522ca6c03a5431d2c010220e0
veranoweb <jay.west@verano.com>
2022-02-02 13:17:40 -0600

    Put logo in picture element to avoid duplicate Alts

--- public/wp-content/themes/zenleaf-custom-theme/template-parts/nav-menu.php
+++ public/wp-content/themes/zenleaf-custom-theme/template-parts/nav-menu.php
@@ -4,28 +4,31 @@
             <div class="absolute inset-y-0 left-0 flex items-center lg:hidden">
                 <!-- Mobile menu button-->
                 <button type="button" class="inline-flex items-center justify-center p-2 ml-4 rounded-md text-black" aria-controls="mobile-menu" aria-expanded="false" id="mobile-menu-button">
-					<span class="sr-only">Open main menu</span>
-					<svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-					</svg>
-					<span class="ml-2 uppercase text-sm sm:text-base">Menu</span>
-				</button>
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                    <span class="ml-2 uppercase text-sm sm:text-base">Menu</span>
+                </button>
             </div>
             <div class="flex-1 flex items-center justify-center lg:items-stretch lg:justify-start">
                 <!-- Logo -->
                 <a class="flex-shrink-0 flex items-center" href="/">
-                    <img class=" md:h-10 w-auto lg:mt-1 hidden md:block" style="max-width:50vw" src="/wp-content/uploads/2022/01/zen-leaf-logo-with-text-67h.svg" alt="Zen Leaf Dispensaries">
-                    <img class="h-10  w-auto md:hidden" style="max-width:50vw" src="/wp-content/uploads/2022/01/zen-leaf-logo-67h.svg" alt="Zen Leaf Dispensaries">
+                    <picture class="h-10 md:h-10 w-auto lg:mt-1 hidden md:block" style="max-width:50vw">
+                        <source media="(min-width: 768px)" srcset="/wp-content/uploads/2022/01/zen-leaf-logo-with-text-67h.svg" alt="Zen Leaf Dispensaries">
+                        <source media="(min-width: 10px)" srcset="/wp-content/uploads/2022/01/zen-leaf-logo-67h.svg" alt="Zen Leaf Dispensaries">
+                        <img src="O/wp-content/uploads/2022/01/zen-leaf-logo-with-text-67h.svg" alt="Zen Leaf Dispensaries">
+                    </picture>
                 </a>
                 <!-- Desktop Menu -->
                 <div class="ml-6 uppercase mt-2 hidden lg:flex masthead-navbar overflow-hidden">
                     <div class="dropdown">
                         <button class="megamenu-button dropbtn" id="about-megamenu-button" aria-haspopup="true">
-							About 
-							<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-							<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-							</svg>
-						</button>
+                            About
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
                         <div class="dropdown-content hidden absolute z-50 bg-white p-4 w-80 -ml-6">
                             <a href="/about" class="nav-sub-link">Our History</a>
                             <a href="/about/careers" class="nav-sub-link">Careers</a>
@@ -34,11 +37,11 @@
                     </div>
                     <div class="dropdown">
                         <button class="megamenu-button dropbtn" id="learn-megamenu-button" aria-haspopup="true">
-							Learn 
-							<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-								<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-							</svg>
-						</button>
+                            Learn
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
                         <div class="dropdown-content hidden absolute z-50 bg-white p-4 w-80 -ml-6">
                             <a href="/learn" class="nav-sub-link">Getting Started</a><a href="/state-resources" class="nav-sub-link">Resources by State</a>
                             <a href="/learn/cannabis-products/" class="nav-sub-link">Cannabis Products</a>
@@ -60,17 +63,16 @@
                         </a>
                         <!-- Shop Button -->
                         <div class="dropdown">
-                            <button class="bg-black hover:bg-white ring-gray-700 border-solid border-2 border-black hover:text-black text-sm py-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white text-white px-4 sm:px-8 uppercase inline-flex items-center rounded-md tracking-widest sm:text-base lg:mt-2 font-bold group"
-                                id="user-menu-button" aria-haspopup="true">
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 hidden sm:block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-								</svg>
-								</p>
-								<p class="">Shop<span class="hidden xl:inline-block">&nbsp;Online</span> </p>
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-3 hidden lg:block" viewBox="0 0 20 20" fill="currentColor">
-								<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-								</svg>
-							</button>
+                            <button class="bg-black hover:bg-white ring-gray-700 border-solid border-2 border-black hover:text-black text-sm py-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white text-white px-4 sm:px-8 uppercase inline-flex items-center rounded-md tracking-widest sm:text-base lg:mt-2 font-bold group" id="user-menu-button" aria-haspopup="true">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 hidden sm:block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+                                </svg>
+                                </p>
+                                <p class="">Shop<span class="hidden xl:inline-block">&nbsp;Online</span> </p>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-3 hidden lg:block" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
                             <div class="dropdown-content origin-top-right absolute right-0 w-80 sm:w-96 shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none hidden z-50 rounded-md mr-4" id="user-menu">
                                 <div id="shop_by_state" height="400"></div>
                             </div>
@@ -85,29 +87,29 @@
                 <div class="w-full rounded-sm shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none py-4 " role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1" id="mobile-menu-root">
                     <a href="/locations" class="nav-link-m" role="menuitem" tabindex="-1">Locations</a>
                     <button type="button" class="megamenu-button-m" role="menuitem" tabindex="-1" id="mobile-menu-about-button">
-						<span class="float-left w-full text-left">
-							About
-							<svg xmlns="http://www.w3.org/2000/svg" stroke="currentColor" class="h-4 w-4 m-1  float-right" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-							</svg>
-						</span>
-					</button>
+                        <span class="float-left w-full text-left">
+                            About
+                            <svg xmlns="http://www.w3.org/2000/svg" stroke="currentColor" class="h-4 w-4 m-1  float-right" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                            </svg>
+                        </span>
+                    </button>
                     <button type="button" class="megamenu-button-m" role="menuitem" tabindex="-1" id="mobile-menu-learn-button">
-						<span class="float-left w-full text-left">
-							Learn
-							<svg xmlns="http://www.w3.org/2000/svg" stroke="currentColor" class="h-4 w-4 m-1  float-right" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-							</svg>
-						</span>
-            		</button>
+                        <span class="float-left w-full text-left">
+                            Learn
+                            <svg xmlns="http://www.w3.org/2000/svg" stroke="currentColor" class="h-4 w-4 m-1  float-right" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                            </svg>
+                        </span>
+                    </button>
                     <a href="/blog" class="nav-link-m" role="menuitem" tabindex="-1">Blog</a>
                     <a href="/contact" class="nav-link-m" role="menuitem" tabindex="-1" id="user-menu-item-4">Contact</a>
                 </div>
                 <div class="mobile-sub-menu" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1" id="mobile-menu-about">
                     <a href="#" class="back-to-main-m return-to-root" role="menuitem" tabindex="-1">
                         <svg class=" mr-2 flex-shrink-0 h-5 w-h-4 w-4 m-1 transform inline-block right-0 rotate-180" stroke="currentColor" fill="white" viewBox="0 0 20 20" aria-hidden="true">
-                  			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-               			</svg> Back
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                        </svg> Back
                     </a>
                     <hr class="mb-4" />
 
@@ -117,8 +119,8 @@
                 <div class="mobile-sub-menu" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1" id="mobile-menu-learn">
                     <a href="#" class="back-to-main-m return-to-root" role="menuitem" tabindex="-1">
                         <svg class="mr-2 flex-shrink-0 h-4 w-4 m-1 transform inline-block right-0 rotate-180" viewBox="0 0 20 20" stroke="currentColor" fill="white" aria-hidden="true">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-						</svg> Back
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                        </svg> Back
                     </a>
                     <hr class="mb-4" />
                     <a href="/about" class="nav-sub-link-m" role="menuitem" tabindex="-1">Getting Started</a>
@@ -176,20 +178,22 @@
     .dropdown:focus .dropdown-content {
         display: block;
     }
-    
+
     #locations-megamenu-button {
         margin-top: 10px;
     }
+
     nav .dropdown button svg {
         margin-top: 2px;
     }
+
     nav .dropdown button#user-menu-button svg {
         margin-top: 0px;
     }
-    @media screen and (min-width: 1280px){
+
+    @media screen and (min-width: 1280px) {
         nav .dropdown button svg {
             margin-top: 6px;
         }
     }
-    
 </style>
